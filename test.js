57a8575b56ea2da6bcbe76a3b0262a767d86de0f
Jess Vanam <32110513+jvanam@users.noreply.github.com>
2023-05-25 13:26:18 -0400

    add mobile nav state

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+// state vars
+let mobileNavShowing = false;
+
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('the dom has loaded!')
+	console.log("the dom has loaded!");
+	let mobileNavToggle = document.getElementById("toggle-mobile-nav-button");
+	console.log(mobileNavToggle);
+	mobileNavToggle.addEventListener("click", () => {
+		mobileNavShowing = !mobileNavShowing;
+		console.log("mobile nav showing: ", mobileNavShowing);
+	});
 });
